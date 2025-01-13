c67603617fd7add48472c52ead41d0378889fbea
Jurabek Abduhalilov <jurabek.tech.abduhalilov@gmail.com>
2025-01-13 17:09:00 +0500

    project done ig

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 
 # Todo List created using only HTML, CSS & JavaScript
 
+![Screenshot](/images/screenshot.png)
+
 This is a personal practice project to learn JavaScript!
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let list = document.querySelector('.todo-list');
 
 // Init Array
 let todos = [];
+let editIndex = -1;
 
 // Handle Functionality
 form.addEventListener('submit', (e) => {
@@ -17,39 +18,63 @@ form.addEventListener('submit', (e) => {
     // Prevent Empty Todo
     if (value.length <= 0) return alert('You need to write smth _-_');
 
-    todos.push(value)
+    if (editIndex >= 0) {
+        todos[editIndex].text = value;
+        editIndex = -1;
+    } else {
+        todos.push({
+            checked: false,
+            text: value
+        });
+    }
 
     createTodoElement()
 })
 
 function createTodoElement() {
-    list.innerHTML = ''
-    todos?.reverse().forEach((todo, index) => {
+    list.innerHTML = '';
+    todos.slice().reverse().forEach((todo, index) => {
+        let realIndex = todos.length - 1 - index;
+
         let todo_task = document.createElement('div');
         todo_task.className = 'todo-task';
-        todo_task.id = `todo-${index}`
-        todo_task.innerHTML = `<div class="todo-check"></div><p class="todo-text">${todo}</p><div class="todo-icons"><span class="material-symbols-outlined todo-edit">edit</span><span class="material-symbols-outlined todo-trash">delete</span></div>`
+        todo_task.id = `todo-${realIndex}`;
+        todo_task.innerHTML = `<div class="todo-check"></div><p class="todo-text">${todo.text}</p><div class="todo-icons"><span class="material-symbols-outlined todo-edit">edit</span><span class="material-symbols-outlined todo-trash">delete</span></div>`;
 
-        list.appendChild(todo_task)
+        list.appendChild(todo_task);
 
-        let check = document.querySelector(`#todo-${index} .todo-check`);
-        let edit = document.querySelector(`#todo-${index} .todo-edit`);
-        let trash = document.querySelector(`#todo-${index} .todo-trash`);
-        let text = document.querySelector(`#todo-${index} .todo-text`);
+        let check = document.querySelector(`#todo-${realIndex} .todo-check`);
+        let edit = document.querySelector(`#todo-${realIndex} .todo-edit`);
+        let trash = document.querySelector(`#todo-${realIndex} .todo-trash`);
+        let text = document.querySelector(`#todo-${realIndex} .todo-text`);
 
         check.addEventListener('click', () => {
             check.classList.toggle('checked');
+            todo.checked = !todo.checked
 
             if (check.classList.contains('checked')) {
-                text.innerHTML = `<del>${todo}</del>`
+                text.innerHTML = `<del>${todo.text}</del>`;
             } else {
-                text.innerHTML = todo
+                text.innerHTML = todo.text;
             }
-        })
+        });
+
+        if (todo.checked) {
+            check.classList.add('checked')
+            text.innerHTML = `<del>${todo.text}</del>`;
+        } else {
+            check.classList.remove('checked')
+            text.innerHTML = todo.text;
+        }
+
+        edit.addEventListener('click', () => {
+            input.value = todo.text;
+            editIndex = realIndex;
+        });
 
         trash.addEventListener('click', () => {
-            todos.splice(index, 1);
+            todos.splice(realIndex, 1);
             createTodoElement();
         });
-    })
+    });
 }
